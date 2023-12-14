8fbf97926c2e9e43ac510d4f8d0aa73178531848
tanvighai <tanvighai02@gmail.com>
2023-12-14 03:22:45 -0500

    done

--- main.tf
+++ main.tf
@@ -2,4 +2,12 @@ module "vpc" {
   source = "./module/vpc"
   vpc_cidr = var.vpc_cidr
   env  = var.env
+  private_subnets        = var.private_subnets
+  public_subnets         = var.public_subnets
+  azs                    = var.azs
+  account_no             = var.account_no
+  default_vpc_id         = var.default_vpc_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
+
--- vars.tf
+++ vars.tf
@@ -1,2 +1,9 @@
 variable "env" {}
 variable "vpc_cidr" {}
+variable "azs" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "account_no" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_route_table_id" {}
